c179cef339fc3250e2e91fa034f4b1a4fd25e931
Arthur Zall <zallarthur@gmail.com>
2024-08-25 20:33:09 -0300

    componentização da lista de cidades

--- src/PaginaPrincipal.vue
+++ src/PaginaPrincipal.vue
@@ -1,49 +1,18 @@
 <template>
     <Cabecalho />
-    <div class="lista">
-      <div class="card" v-for="cidade in cidades" :key="cidade.id">
-        <div class="card-img"></div>
-        <h3>{{ cidade.name }}</h3>
-        <h4>{{ cidade.description }}</h4>
-        <div class="temperatura">{{ cidade.temp }}º</div>
-      </div>
-    </div>
+    <ListaDeCidades/>
 </template>
 
 <script>
 
 import './assets/estilo.css'
 import Cabecalho from './components/Cabecalho.vue';
+import ListaDeCidades from './components/ListaDeCidades.vue';
+
 
 export default {
-    components: { Cabecalho },
-    data() {
-        return {
-        cidades: [
-            {
-            id: 8429,
-            name: "Rio de Janeiro",
-            description: "Ensolarado",
-            icon: "01d",
-            temp: "25",
-            },
-            {
-            id: 8412,
-            name: "Manaus",
-            description: "Chuvas Fortes",
-            icon: "09d",
-            temp: "25",
-            },
-            {
-            id: 8454,
-            name: "Brasília",
-            description: "Ensolarado",
-            icon: "10d",
-            temp: "27.4",
-            },
-        ],
-        };
-  },
+    components: { Cabecalho, ListaDeCidades },
+    
 }
 </script>
 
